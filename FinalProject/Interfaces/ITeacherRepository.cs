00752c55dbfadbb9c22938e123dafacf3f3e414d
Gunel2727 <gunelemu@code.edu.az>
2026-06-18 23:29:17 +0400

    added new code

---
+++ FinalProject/Interfaces/ITeacherRepository.cs
@@ -0,0 +1,13 @@
+﻿using StudentInformationSystem.Domain.Models;
+
+namespace StudentInformationSystem.Domain.Interfaces
+{
+    public interface ITeacherRepository
+    {
+        Task<Teacher?> GetByIdAsync(int id);
+        Task<IEnumerable<Teacher>> GetAllAsync();
+        Task AddAsync(Teacher teacher);
+        void Update(Teacher teacher);
+        void Delete(Teacher teacher);
+    }
+}
--- FinalProject/obj/Release/net8.0/StudentInformationSystem.Domain.AssemblyInfo.cs
+++ FinalProject/obj/Release/net8.0/StudentInformationSystem.Domain.AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("StudentInformationSystem.Domain")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+13c758d32d0f2d6d94b7b963f283a2b77afd43fd")]
 [assembly: System.Reflection.AssemblyProductAttribute("StudentInformationSystem.Domain")]

3488362a1378a14a362ddaa3e279a79f10a1c6b2
Gunel2727 <gunelemu@code.edu.az>
2026-06-19 23:33:42 +0400

    added new code

---
+++ FinalProject/Interfaces/ICourseRepository.cs
@@ -0,0 +1,15 @@
+﻿using StudentInformationSystem.Domain.Models;
+
+namespace StudentInformationSystem.Domain.Interfaces
+{
+    public interface ICourseRepository
+    {
+        Task<Course?> GetByIdAsync(int id);
+        Task<IEnumerable<Course>> GetAllAsync();
+       
+        Task<IEnumerable<Course>> GetByTeacherIdAsync(int teacherId);
+        Task AddAsync(Course course);
+        void Update(Course course);
+        void Delete(Course course);
+    }
+}

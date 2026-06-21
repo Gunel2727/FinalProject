dc85761aa07df8e4468e61276259d1f962922818
Gunel2727 <gunelemu@code.edu.az>
2026-06-21 23:45:56 +0400

    an interface is added

---
+++ SIS.Application/Interfaces/IStudentService.cs
@@ -0,0 +1,18 @@
+﻿using SIS.Application.DTOs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SIS.Application.Interfaces
+{
+    public interface IStudentService
+    {
+        Task<IList<StudentDto>> GetAllAsync();
+        Task<StudentDto> GetByIdAsync(int id);
+        Task<StudentDto> CreateAsync(CreateStudentDto dto);
+        Task<StudentDto> UpdateAsync(int id, UpdateStudentDto dto);
+        Task DeleteAsync(int id);
+    }
+}

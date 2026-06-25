711952a358b818a120ef141ef2821013b6dc196c
Gunel2727 <gunelemu@code.edu.az>
2026-06-25 23:28:47 +0400

    a new interface is added

---
+++ SIS.Application/Interfaces/IEnrollmentService.cs
@@ -0,0 +1,16 @@
+﻿using SIS.Application.DTOs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SIS.Application.Interfaces
+{
+    public interface IEnrollmentService
+    {
+        Task<IList<EnrollmentDto>> GetByStudentIdAsync(int studentId);
+        Task<EnrollmentDto> EnrollAsync(CreateEnrollmentDto dto);
+        Task UnenrollAsync(int enrollmentId);
+    }
+}
